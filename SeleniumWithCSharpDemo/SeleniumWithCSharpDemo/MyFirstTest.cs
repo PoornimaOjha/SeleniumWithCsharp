81a114f11670b1288ad06d3b5c3546dd661adb9b
PoornimaOjha <erpoornimaojha@gmail.com>
2021-10-03 23:19:08 -0400

    first test

---
+++ SeleniumWithCSharpDemo/SeleniumWithCSharpDemo/MyFirstTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWithCSharpDemo
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("ojha");
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+            string title = driver.Title;
+
+            Assert.AreEqual("ojha - Google search", title);
+        }
+
+        [TearDown]
+        public void Clean()
+        {
+
+        }
+    }
+}
